e9a7e06b2f3db52b4b1b34ed662356ee76810be4
andradek19 <andradeka1900@gmail.com>
2020-11-19 04:57:34 -0500

    Add files via upload

--- SplashMountainControlSystem.java
+++ SplashMountainControlSystem.java
@@ -1,9 +1,33 @@
+package cis461.project;
 
+
+/**
+ * CIS 461 Project
+ * 
+ * SplashMountainControlSystem sets up how many seats the ride has and how many passengers there are.
+ * Then, it sets up an instance of the monitor Log, then generates and starts m Passenger threads, which then independently 
+ * run to show the behavior of the system.
+ */
 public class SplashMountainControlSystem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n = 6;
+		int m = 18;
+		System.out.println("How many seats in the ride? (n = "+n+")");
+		
+		System.out.println("How many passengers total? (m = "+m+")");
+		
+		
+		Log log1 = new Log(n);
+		
+		//generates array of size m, then initializes and starts m threads
+		Thread passengers[] = new Thread[m];
+		for(int x = 0; x < m; x++){
+			passengers[x] = new Thread(new Passenger(log1));
+		}
+		for(int x = 0; x < m; x++){
+			passengers[x].start();
+		}
 	}
 
 }
